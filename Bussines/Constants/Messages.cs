3e503d266488a38771858d7d8cd2c8450280c5b1
Alparslan <alparslanaydgn02@gmail.com>
2022-12-10 22:52:37 +0300

    Added JWT

--- Bussines/Constants/Messages.cs
+++ Bussines/Constants/Messages.cs
@@ -24,7 +24,7 @@ namespace Bussines.Constants
         public static string UserRegistered = "User registered";
         public static string UserNotFound = "User Not Found";
         public static string PasswordError = "Password Error";
-        public static string SuccessfulLogin = "Successful Login";
+        public static string SuccessfulLogin = "Sucessful Login";
         public static string UserAlreadyExists = "User Already Exists";
         public static string AccessTokenCreated = "Access Token Created";
     }
