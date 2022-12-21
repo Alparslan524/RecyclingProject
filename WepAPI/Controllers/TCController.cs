858bd9bcc96b5a259f95a9f5c059bdb118a2f139
Alparslan <alparslanaydgn02@gmail.com>
2022-12-21 15:27:15 +0300

    Added MERNİS control system

---
+++ WepAPI/Controllers/TCController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TCController : ControllerBase
+    {
+        bool status;
+        public async void TcDogrula(string ad, string soyad, long tc, int dogumTarih)
+        {
+            using (tcNoDogrula.KPSPublicSoapClient servis = new tcNoDogrula.KPSPublicSoapClient(tcNoDogrula.KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap))
+            {
+                var response = await servis.TCKimlikNoDogrulaAsync(tc, ad.ToUpper(), soyad.ToUpper(), dogumTarih);
+                status = response.Body.TCKimlikNoDogrulaResult;
+                
+            }
+        }
+
+
+        [HttpGet("tcdogrula")]
+        public bool TcDogrulamaFrontEnd(string ad, string soyad, long tc, int dogumTarih)
+        {
+            TcDogrula(ad, soyad, tc, dogumTarih);
+            Thread.Sleep(2000);
+            return status;
+        }
+
+
+    }
+}
